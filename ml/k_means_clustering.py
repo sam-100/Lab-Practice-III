28986fc56421b64561896b73ed9b00adb8e32d16
sam-100 <76462898+sam-100@users.noreply.github.com>
2023-11-20 07:26:13 +0530

    Update k_means_clustering.py

--- ml/k_means_clustering.py
+++ ml/k_means_clustering.py
@@ -1,75 +1,54 @@
-import pandas as pd
+# Problem Statement:
+# Implement K-Means clustering/ hierarchical clustering on sales_data_sample.csv dataset.
+# Determine the number of clusters using the elbow method.
+
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from scipy.cluster.hierarchy import dendrogram, linkage
+
+df = pd.read_csv('sales_data_sample.csv', encoding="unicode_escape")
+
+df.head()
+
+df.dtypes
+
+df.shape
+
+df.isna().sum()
+
+df['ADDRESSLINE2'] = df['ADDRESSLINE2'].fillna('NA')
+df['STATE'] = df['STATE'].fillna('NA')
+df = df.dropna(axis = 0, subset=['POSTALCODE', 'TERRITORY'])
+
+df.isna().sum()
+
+df_numeric = df.select_dtypes(include=[np.number])
+
+df_numeric
+
+# Scaling and Normalization
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import silhouette_score
-from sklearn.cluster import AgglomerativeClustering
-
-# Load the dataset
-df = pd.read_csv("sales_data_sample.csv")  # Update with the actual file path
-
-# Display the first few rows of the dataset
-print(df.head())
-
-# Extract relevant features for clustering
-features = df[['Sales', 'Quantity']]
-
-# Standardize the features
 scaler = StandardScaler()
-features_scaled = scaler.fit_transform(features)
+df_scaled = scaler.fit_transform(df_numeric)
 
-# K-Means clustering and the Elbow Method
-inertia_values = []
-possible_k_values = range(1, 11)
+df_scaled
 
-for k in possible_k_values:
-    kmeans = KMeans(n_clusters=k, random_state=42)
-    kmeans.fit(features_scaled)
-    inertia_values.append(kmeans.inertia_)
+# K means clustering
+from sklearn.cluster import KMeans
 
-# Plotting the Elbow Method graph
-plt.plot(possible_k_values, inertia_values, marker='o')
-plt.title('Elbow Method for Optimal k (K-Means)')
-plt.xlabel('Number of Clusters (k)')
+inertia = []
+k_min, k_max = 1, 21
+
+for i in range(k_min, k_max):
+    kmeans = KMeans(n_clusters=i, random_state=45, n_init=10)
+    kmeans.fit(df_scaled)
+    inertia.append(kmeans.inertia_)
+inertia
+
+plt.plot(range(k_min, k_max), inertia, marker='o')
+plt.xlabel('Number of clusters')
 plt.ylabel('Inertia')
+plt.title('Elbow method for optimal K')
 plt.show()
 
-# Hierarchical Clustering and Dendrogram
-linked = linkage(features_scaled, method='ward')
-dendrogram(linked, orientation='top', distance_sort='descending', show_leaf_counts=True)
-plt.title('Hierarchical Clustering Dendrogram')
-plt.xlabel('Sample Index')
-plt.ylabel('Cluster Distance')
-plt.show()
 
-# Determine the optimal number of clusters using Silhouette Score for K-Means
-silhouette_scores = []
-
-for k in possible_k_values:
-    kmeans = KMeans(n_clusters=k, random_state=42)
-    labels = kmeans.fit_predict(features_scaled)
-    silhouette_scores.append(silhouette_score(features_scaled, labels))
-
-# Plotting Silhouette Scores for K-Means
-plt.plot(possible_k_values, silhouette_scores, marker='o')
-plt.title('Silhouette Score for Optimal k (K-Means)')
-plt.xlabel('Number of Clusters (k)')
-plt.ylabel('Silhouette Score')
-plt.show()
-
-# Determine the optimal number of clusters using Silhouette Score for Hierarchical Clustering
-silhouette_scores_hierarchical = []
-
-for k in possible_k_values:
-    hierarchical = AgglomerativeClustering(n_clusters=k)
-    labels = hierarchical.fit_predict(features_scaled)
-    silhouette_scores_hierarchical.append(silhouette_score(features_scaled, labels))
-
-# Plotting Silhouette Scores for Hierarchical Clustering
-plt.plot(possible_k_values, silhouette_scores_hierarchical, marker='o')
-plt.title('Silhouette Score for Optimal k (Hierarchical Clustering)')
-plt.xlabel('Number of Clusters (k)')
-plt.ylabel('Silhouette Score')
-plt.show()
