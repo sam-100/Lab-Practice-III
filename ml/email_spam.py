374eb899e94b2a08a1cd08527eacde18fae03ab5
sam-100 <76462898+sam-100@users.noreply.github.com>
2023-11-20 07:04:08 +0530

    Update email_spam.py

--- ml/email_spam.py
+++ ml/email_spam.py
@@ -1,51 +1,79 @@
-# Import necessary libraries
+# Problem Statement
+# Classify the email using the binary classification method. Email Spam detection has two
+# states: a) Normal State – Not Spam, b) Abnormal State – Spam. Use K-Nearest Neighbors and
+# Support Vector Machine for classification. Analyze their performance.
+# Dataset link: The emails.csv dataset on the Kaggle
+
+# Import libraries
+import numpy as np
 import pandas as pd
+import sklearn as skl
+
+# Read the data and perform preprocessing
+
+df = pd.read_csv('emails.csv')
+df
+
+df.head()
+
+df.dtypes
+
+df.drop(['Email No.'], axis = 1, inplace=True)
+
+df
+
+df.isna().sum()
+
+# Split the dataset into feature-set (X) and target-set (y)
+
+X = df.drop(['Prediction'], axis = 1)
+y = df['Prediction']
+
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=40)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# KNN Classifier
 from sklearn.neighbors import KNeighborsClassifier
+
+# 1. Train
+knn_classifier = KNeighborsClassifier(n_neighbors=10)
+knn_classifier.fit(X_train_scaled, y_train)
+
+# 2. Predict
+y_pred_knn = knn_classifier.predict(X_test_scaled)
+y_pred_knn
+
+# SVM Classifier
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-# Load the dataset
-df = pd.read_csv("emails.csv")
-
-# Display the first few rows of the dataset
-print(df.head())
-
-# Separate features (X) and target variable (y)
-X = df['text']
-y = df['spam']
-
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Task 1: Preprocess the text data
-vectorizer = TfidfVectorizer(max_features=5000)
-X_train_tfidf = vectorizer.fit_transform(X_train)
-X_test_tfidf = vectorizer.transform(X_test)
-
-# Task 2: Train K-Nearest Neighbors (KNN) classifier
-knn_classifier = KNeighborsClassifier(n_neighbors=5)
-knn_classifier.fit(X_train_tfidf, y_train)
-
-# Predictions on the test set
-knn_predictions = knn_classifier.predict(X_test_tfidf)
-
-# Evaluate KNN performance
-print("K-Nearest Neighbors (KNN) Performance:")
-print("Accuracy:", accuracy_score(y_test, knn_predictions))
-print("Classification Report:\n", classification_report(y_test, knn_predictions))
-print("Confusion Matrix:\n", confusion_matrix(y_test, knn_predictions))
-
-# Task 3: Train Support Vector Machine (SVM) classifier
+# 1. Train
 svm_classifier = SVC(kernel='linear')
-svm_classifier.fit(X_train_tfidf, y_train)
+svm_classifier.fit(X_train_scaled, y_train)
 
-# Predictions on the test set
-svm_predictions = svm_classifier.predict(X_test_tfidf)
+# 2. Predict
+y_pred_svm = svm_classifier.predict(X_test_scaled)
+y_pred_svm
 
-# Evaluate SVM performance
-print("\nSupport Vector Machine (SVM) Performance:")
-print("Accuracy:", accuracy_score(y_test, svm_predictions))
-print("Classification Report:\n", classification_report(y_test, svm_predictions))
-print("Confusion Matrix:\n", confusion_matrix(y_test, svm_predictions))
+# Performance Evaluation
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+accuracy_knn = accuracy_score(y_test, y_pred_knn)
+accuracy_svm = accuracy_score(y_test, y_pred_svm)
+print('Accuracy of KNN = ', accuracy_knn)
+print('Accuracy of SVM = ', accuracy_svm)
+
+print('KNN\'s classification report: ')
+print(classification_report(y_test, y_pred_knn))
+print('SVM\'s classification report: ')
+print(classification_report(y_test, y_pred_svm))
+
+print('KNN\'s confusion matrix')
+print(confusion_matrix(y_test, y_pred_knn))
+print('SVM\'s confusion matrix')
+print(confusion_matrix(y_test, y_pred_svm))
